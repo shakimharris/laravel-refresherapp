15cfe51a083d7686f11ced07729963eb42913308
Shakim Harris <sharris@gls.us.com>
2019-07-26 15:31:57 -0400

    updated route

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('about', 'AboutController@index');
 
 });
