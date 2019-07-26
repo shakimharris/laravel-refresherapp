1b2f0cdff50915ac009bf252e655633a0da93e02
Shakim Harris <sharris@gls.us.com>
2019-07-26 14:54:48 -0400

    updated about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+         <h2> I think this page is the about page </h2> 
+
+
+        </body>
+        </html>
