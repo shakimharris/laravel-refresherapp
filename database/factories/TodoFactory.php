684979040fe53dd8702c251259f104511d639669
Shakim Harris <sharris@gls.us.com>
2019-07-29 14:17:04 -0400

    added array to factory

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define('App\Todo::class', function (Faker $faker) {
     return [
-        //
+        'name' => $faker->sentence(3),
+        'description' => $faker->paragraph(4),
+        'completed' => false 
     ];
 });
