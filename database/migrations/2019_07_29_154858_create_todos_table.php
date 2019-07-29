45129b43a8016c49db39c027ebe77bea29171eeb
Shakim Harris <sharris@gls.us.com>
2019-07-29 12:15:08 -0400

    added completed boolean to table

--- database/migrations/2019_07_29_154858_create_todos_table.php
+++ database/migrations/2019_07_29_154858_create_todos_table.php
@@ -17,6 +17,7 @@ class CreateTodosTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->text('description');
+            $table->boolean('completed');
             $table->timestamps();
         });
     }
