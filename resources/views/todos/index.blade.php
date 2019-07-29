3716b4591219cb44287c2769896a1ba98ead3bd9
Shakim Harris <sharris@gls.us.com>
2019-07-29 14:52:27 -0400

    placed todos inside a card

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,17 +1,22 @@
 <html>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
 <title>TODOS</title>
+<body>
 <h1 class="text=center my-5"> TODOS PAGE </h1>
+<div class="card card-default">
+    <div class="card-header">Todos </div>
+        <div class="card-body">
+          @foreach($todos as $todo)
+           <ul class="list-group">
+             <li class="list-group-item"> 
 
-@foreach($todos as $todo)
-   <ul class="list-group">
-    <li class="list-group-item"> 
+              {{ $todo->name }}
 
-    {{ $todo->name }}
+             </li>
 
-    </li>
-    
-
-    @endforeach
-  </ul> 
+         @endforeach
+       </ul> 
+     </div>
+    </div>
+  </body>
 </html>
