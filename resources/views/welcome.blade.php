a70bea1552a6b9491b5d93c594b4b213fa4ccc3b
Shakim Harris <sharris@gls.us.com>
2019-07-29 11:23:00 -0400

    added todos link to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Homepage 
+                    <a href="/todos">To Dos </a> 
                 </div>
 
            \\
