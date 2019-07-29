07f4e2622a4795971ef97e3da7cb1030a9de7974
Shakim Harris <sharris@gls.us.com>
2019-07-29 15:00:41 -0400

    centered list and added offset

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -3,9 +3,10 @@
 <title>TODOS</title>
 <body>
 <h1 class="text=center my-5"> TODOS PAGE </h1>
-
-<div class="container">
- <div class="card card-default">
+<div class="row justify-content-center">
+ <div class="col-md-8.offset-md-2">
+  <div class="container">
+   <div class="card card-default">
     <div class="card-header">Todos </div>
         <div class="card-body">
           @foreach($todos as $todo)
@@ -21,6 +22,8 @@
      </div>
     </div>
    </div>
- </div>
+  </div>
+  </div>
+  </div>
   </body>
 </html>
