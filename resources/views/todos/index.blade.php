c03dffbc2b750f3975f4d18d2657ad5215d1468d
Shakim Harris <sharris@gls.us.com>
2019-07-29 14:45:58 -0400

    added margin and centered

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,7 +1,7 @@
 <html>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
 <title>TODOS</title>
-<h1> TODOS PAGE </h1>
+<h1 class="text=center my-5"> TODOS PAGE </h1>
 
 @foreach($todos as $todo)
    <ul>
