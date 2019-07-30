6efe2d1cbf23930beb297d66986ccd6829d620b5
Shakim Harris <sharris@gls.us.com>
2019-07-30 09:35:46 -0400

    fixed links

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -8,7 +8,7 @@
 
 <div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -16,7 +16,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/todos">Todos <span class="sr-only">(current)</span></a>
       </li>
       
     </form>
