b05c10846e074db75a089a9b27f0efbe5a55b860
Shakim Harris <sharris@gls.us.com>
2019-07-30 09:57:49 -0400

    added card to form

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -2,21 +2,25 @@
 
 
 @section('content')
-
- <h1 class="text-center my-5">Create Todos </h1>
-  <div class="row justify-content-center">
-   <div class="col-md-8">
-<form action="">
-
- <div class="form-group">
-    
-    <input type="text" class="form-control" name="name">
-
-
+<h1 class="text-center my-5">Create Todos </h1>
+ <div class="card card-default">
+  <div class="card-header">
+  Create a new todo 
     </div>
-   </div>
-  </div>
-  </form> 
+     <div class="card-body">
+      <div class="row justify-content-center">
+       <div class="col-md-8">
+        <form action="">
 
+         <div class="form-group">
+    
+           <input type="text" class="form-control" name="name">
+                    
+                   </form> 
+                   </div>
+                  </div>
+               </div>
+            </div> 
+         </div>
 
 @endsection
