5da6a7b458368c79eca964c495670b8ea4c4293d
Shakim Harris <sharris@gls.us.com>
2019-07-30 10:57:54 -0400

    added error dynamically

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -13,8 +13,14 @@
         @if($errors->any())
          <div class="alert alert-danger">
           <ul class="list-group">
-          @foreach($errors->all() as $error)
+          @foreach($errors->all() as $error) 
 
+           <li class="list-group-item">
+
+            {{ $error }}
+
+         </li>
+ 
           @endforeach
           </ul>
          </div>
