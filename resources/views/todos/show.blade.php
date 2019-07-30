52b3d1a0baa97be616bc1d2c834b1e70e80bf0fd
Shakim Harris <sharris@gls.us.com>
2019-07-30 08:52:49 -0400

    created view for "show"

---
+++ resources/views/todos/show.blade.php
@@ -0,0 +1,23 @@
+<html>
+<title></title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+<body>
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
