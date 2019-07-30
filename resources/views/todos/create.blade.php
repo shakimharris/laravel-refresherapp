976e8170d572d0ee75c09a457e9679094202ff42
Shakim Harris <sharris@gls.us.com>
2019-07-30 10:32:24 -0400

    added csrf

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -11,21 +11,20 @@
       <div class="row justify-content-center">
        <div class="col-md-8">
         <form action="/store-todos" method="POST">
-
+         @csrf
          <div class="form-group">
     
            <input type="text" class="form-control" placeholder="Name" name="name">
                         </div>
-                       </form> 
              <div class="form-group">
               <textarea name="description" placeholder="Description" id="" cols="30" rows="10" class="form-control"></textarea>
               </div>
 
               <div class="form-group text-center">
                <button type="submit" class="btn btn-info">Create todo</button> 
-
-               </div>
              
+               </div>
+               </form>
                    </div>
                   </div>
                </div>
