f3c5f86d0665367d7024622aa4972a6051fb9487
Shakim Harris <sharris@gls.us.com>
2019-07-30 09:10:09 -0400

    added styling to individual todo

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -3,10 +3,13 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
 <body>
 
-
+<div class="container">
+ <h1 class="text-center my-5">
 {{ $todo->name }}
+</h1>
 
 
+</div>
 </body>
 
 
