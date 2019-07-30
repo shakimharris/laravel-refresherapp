02c6ca54da3148f139dca5929965b956b89ffff3
Shakim Harris <sharris@gls.us.com>
2019-07-30 09:21:45 -0400

    added card to display todo desc

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -7,20 +7,20 @@
  <h1 class="text-center my-5">
 {{ $todo->name }}
 </h1>
+  <div class="card card-default">
+   <div class="card-header">
+        Details
+      </div>
+    
+      <div class="card-body">
+
+    {{ $todo->description }}
 
 
-</div>
+
+    
+     </div>
+    </div>
+   </div>
 </body>
-
-
-
-
-
-
-
-
-
-
-
-
 </html>
