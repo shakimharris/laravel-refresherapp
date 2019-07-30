bb721333acb1915cc87bca9f11b9308b4d81468c
Shakim Harris <sharris@gls.us.com>
2019-07-30 08:54:16 -0400

    called individual todo using name property

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,10 +1,10 @@
 <html>
-<title></title>
+<title>To do item</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
 <body>
 
 
-
+{{ $todo->name }}
 
 
 </body>
