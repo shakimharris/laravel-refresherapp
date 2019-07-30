e928c3e3e7275bf01f9594d36e3d7ef3c1addbe3
Shakim Harris <sharris@gls.us.com>
2019-07-30 09:27:16 -0400

    created layout for the app

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>To do</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+</head>
+
+<body>
+
+<div class="container">
+
+    @yield('content');
+
+</div>
+
+
+</body>
+
+</html>
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,6 +1,5 @@
 <html>
-<title>To do item</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+
 <body>
 
 <div class="container">
