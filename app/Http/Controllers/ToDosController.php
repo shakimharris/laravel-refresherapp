4e554b6df67c28f8494fdafd558d757a1c9dab5f
Shakim Harris <sharris@gls.us.com>
2019-07-30 11:55:44 -0400

    completed update req

--- app/Http/Controllers/ToDosController.php
+++ app/Http/Controllers/ToDosController.php
@@ -58,7 +58,7 @@ class ToDosController extends Controller
 
     }
 
-    public function update()
+    public function update($todoId)
     {
 
         $this->validate(request(), [
@@ -68,6 +68,16 @@ class ToDosController extends Controller
         ]);
         
         $data = request()->all();
+
+        $todo = Todo::find($todoId);
+
+        $todo->name = $data['name'];
+
+        $todo->description = $data['description'];
+
+        $todo->save();
+
+        return redirect('/todos');
 
 
     }
